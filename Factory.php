2e02473ea593466e100dd4ab4ee610708d898e31
Ivan Mezinov <ivanmezinov@mail.ru>
2024-06-26 03:27:14 +0300

    add PHPDoc

--- Factory.php
+++ Factory.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Phppride\Documentor\Contracts;
 
+/**
+ * Factory Abstraction
+ */
 interface Factory
 {
+    /**
+     * @param Token $token      Token for processing
+     * @return Node             Document node for subsequent processing
+     */
     public function create(Token $token): Node;
 }
